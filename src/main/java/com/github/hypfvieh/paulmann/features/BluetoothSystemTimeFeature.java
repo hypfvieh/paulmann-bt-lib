03e47b7dd71463602a20aa1a28e093c5b4c31054
David M <maniac@case-of.org>
2017-02-28 14:34:09 +0100

    Added missing SystemTime and Timer features

--- src/main/java/com/github/hypfvieh/paulmann/features/BluetoothSystemTimeFeature.java
+++ src/main/java/com/github/hypfvieh/paulmann/features/BluetoothSystemTimeFeature.java
@@ -1,43 +1,50 @@
 package com.github.hypfvieh.paulmann.features;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import com.github.hypfvieh.bluetooth.wrapper.BluetoothGattCharacteristic;
 
-public class BluetoothSystemTimeFeature extends AbstractBluetoothIntValFeature {
+public class BluetoothSystemTimeFeature extends AbstractBluetoothFeature {
 
     public BluetoothSystemTimeFeature(BluetoothGattCharacteristic _char) {
         super(_char);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public byte getStepSize() {
-        return 1;
-    }
 
     /**
-     * {@inheritDoc}
+     * Setup current date and time to the device.
+     *
+     * @param _date
+     * @return true on success, false otherwise
      */
-    @Override
-    public byte getDefaultValue() {
-        return 0;
-    }
+    public boolean setDateTime(Date _date) {
+        if (_date == null) {
+            return false;
+        }
+        Calendar cal = GregorianCalendar.getInstance();
+        cal.setTime(_date);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public byte getMinValue() {
-        return 0;
-    }
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int min = cal.get(Calendar.MINUTE);
+        int sec = cal.get(Calendar.SECOND);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public byte getMaxValue() {
-        return 1;
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        byte[] dateArr = new byte[7];
+
+        dateArr[0] = (byte) (year % 256); // remainder (e.g. 225 in 2017)
+        dateArr[1] = (byte) (year / 256); // result of division (e.g. 7 in 2017)
+        dateArr[2] = (byte) month;
+        dateArr[3] = (byte) day;
+        dateArr[4] = (byte) hour;
+        dateArr[5] = (byte) min;
+        dateArr[6] = (byte) sec;
+
+        return writeValue(dateArr);
     }
 
     /**
--- src/main/java/com/github/hypfvieh/paulmann/features/BluetoothTimerOnOffWithOptionsFeature.java
+++ src/main/java/com/github/hypfvieh/paulmann/features/BluetoothTimerOnOffWithOptionsFeature.java
@@ -2,47 +2,225 @@ package com.github.hypfvieh.paulmann.features;
 
 import com.github.hypfvieh.bluetooth.wrapper.BluetoothGattCharacteristic;
 
-public class BluetoothTimerOnOffWithOptionsFeature extends AbstractBluetoothIntValFeature {
+public class BluetoothTimerOnOffWithOptionsFeature extends AbstractBluetoothFeature {
 
-    public static final int TIMEROPTION_FADE_OFF = 0;
-    public static final int TIMEROPTION_FADE_1SEC = 1;
-    public static final int TIMEROPTION_FADE_1MIN = 2;
+    public static final int TIMEROPTION_FADE_OFF   = 0;
+    public static final int TIMEROPTION_FADE_1SEC  = 1;
+    public static final int TIMEROPTION_FADE_1MIN  = 2;
     public static final int TIMEROPTION_FADE_10MIN = 3;
+
+    private boolean         startTimeEnabled = false;
+    private boolean         stopTimeEnabled = false;
+
+    private byte            stopTimeHour = 22;
+    private byte            stopTimeMinute = 0;
+
+    private byte            startTimeHour = 20;
+    private byte            startTimeMinute = 0;
+
+    private FadeOption      startFadingOption      = FadeOption.OFF;
+    private FadeOption      stopFadingOption       = FadeOption.OFF;
 
     public BluetoothTimerOnOffWithOptionsFeature(BluetoothGattCharacteristic _char) {
         super(_char);
     }
 
     /**
-     * {@inheritDoc}
+     * Read the current timer setup from the device.
+     * This may fail, which means no settings will be available.
      */
-    @Override
-    public byte getStepSize() {
-        return 1;
+    public void readTimerOptions() {
+        byte[] readValue = readValue();
+        if (readValue.length == 8) {
+            startTimeEnabled = readValue[0] > 0;
+            startTimeHour = readValue[1];
+            startTimeMinute = readValue[2];
+            startFadingOption = FadeOption.values()[readValue[3]];
+
+            stopTimeEnabled = readValue[4] > 0;
+            stopTimeHour = readValue[5];
+            stopTimeMinute = readValue[6];
+            stopFadingOption = FadeOption.values()[readValue[7]];
+        }
+
     }
 
     /**
-     * {@inheritDoc}
+     * Send the currently configured timer options (for both, start and stop timer) to the device.
+     * @return true on success, false otherwise
      */
-    @Override
-    public byte getDefaultValue() {
-        return 0;
+    public boolean writeTimerOptions() {
+        if (!validateHour(startTimeHour) || !validateHour(stopTimeHour)) {
+            logger.debug("startTimeHour = " + startTimeHour + " or stopTimeHour = " + stopTimeHour + " are out of range (0-23)");
+            return false;
+        }
+        if (!validateMinute(startTimeMinute) || !validateMinute(stopTimeMinute)) {
+            logger.debug("startTimeMinute = " + startTimeMinute + " or stopTimeMinute = " + stopTimeMinute + " are out of range (0-59)");
+            return false;
+        }
+
+        byte[] timerOpts = new byte[8];
+
+        timerOpts[0] = isStartTimeEnabled() ? (byte) 1 : 0;
+        timerOpts[1] = startTimeHour;
+        timerOpts[2] = startTimeMinute;
+        timerOpts[3] = (byte) startFadingOption.ordinal();
+
+        timerOpts[4] = isStopTimeEnabled() ? (byte) 1 : 0;
+        timerOpts[5] = stopTimeHour;
+        timerOpts[6] = stopTimeMinute;
+        timerOpts[7] = (byte) stopFadingOption.ordinal();
+
+        return writeValue(timerOpts);
     }
 
     /**
-     * {@inheritDoc}
+     * Helper to validate hours.
+     * @param _hour
+     * @return
      */
-    @Override
-    public byte getMinValue() {
-        return 0;
+    private boolean validateHour(byte _hour) {
+        return (_hour > 23 || _hour < 0);
     }
 
     /**
-     * {@inheritDoc}
+     * Helper to validate minutes.
+     * @param _minute
+     * @return
      */
-    @Override
-    public byte getMaxValue() {
-        return 3;
+    private boolean validateMinute(byte _minute) {
+        return (_minute < 0 || _minute > 59);
+    }
+
+    /**
+     * Setup the start time (enable lights).
+     * @param _hour when to start
+     * @param _minute when to stop
+     */
+    public void setStartTime(byte _hour, byte _minute) {
+        if (!validateHour(_hour)) {
+            return;
+        }
+        if (!validateMinute(_minute)) {
+            return;
+        }
+
+        startTimeHour = _hour;
+        startTimeMinute = _minute;
+    }
+
+    /**
+     * Setup the stop time (disable lights).
+     *
+     * @param _hour hour when to stop
+     * @param _minute minute when to stop
+     */
+    public void setStopTime(byte _hour, byte _minute) {
+        if (_hour > 23 || _hour < 0) {
+            return;
+        }
+        if (_minute < 0 || _minute > 59) {
+            return;
+        }
+
+        stopTimeHour = _hour;
+        stopTimeMinute = _minute;
+    }
+
+    /**
+     * Check if start timer is enabled.
+     * @return
+     */
+    public boolean isStartTimeEnabled() {
+        return startTimeEnabled;
+    }
+
+    /**
+     * Enable/disable the start timer.
+     * @param _startTimeEnabled
+     */
+    public void setStartTimeEnabled(boolean _startTimeEnabled) {
+        startTimeEnabled = _startTimeEnabled;
+    }
+
+    /**
+     * Check if stop timer is enabled.
+     * @return
+     */
+    public boolean isStopTimeEnabled() {
+        return stopTimeEnabled;
+    }
+
+    /**
+     * Enable/disable the stop timer.
+     * @param _stopTimeEnabled
+     */
+    public void setStopTimeEnabled(boolean _stopTimeEnabled) {
+        stopTimeEnabled = _stopTimeEnabled;
+    }
+
+    /**
+     * The current fading option configured for the start timer event.
+     * @param _stopFadingOption
+     */
+    public FadeOption getStartFadingOption() {
+        return startFadingOption;
+    }
+
+    /**
+     * Setup the Fading options to use when start timer event is fired.
+     * @param _stopFadingOption
+     */
+    public void setStartFadingOption(FadeOption _startFadingOption) {
+        startFadingOption = _startFadingOption;
+    }
+
+    /**
+     * The current fading option configured for the stop timer event.
+     * @param _stopFadingOption
+     */
+    public FadeOption getStopFadingOption() {
+        return stopFadingOption;
+    }
+
+    /**
+     * Setup the Fading options to use when stop timer event is fired.
+     * @param _stopFadingOption
+     */
+    public void setStopFadingOption(FadeOption _stopFadingOption) {
+        stopFadingOption = _stopFadingOption;
+    }
+
+    /**
+     * Hour which is configured to stop the timer.
+     * @return
+     */
+    public byte getStopTimeHour() {
+        return stopTimeHour;
+    }
+
+    /**
+     * Minute which is configured to stop the timer.
+     * @return
+     */
+    public byte getStopTimeMinute() {
+        return stopTimeMinute;
+    }
+
+    /**
+     * Hour which is configured to start the timer.
+     * @return
+     */
+    public byte getStartTimeHour() {
+        return startTimeHour;
+    }
+
+    /**
+     * Minute which is configured to start the timer.
+     * @return
+     */
+    public byte getStartTimeMinute() {
+        return startTimeMinute;
     }
 
     /**
@@ -53,4 +231,10 @@ public class BluetoothTimerOnOffWithOptionsFeature extends AbstractBluetoothIntV
         return FeatureIdent.PAULMANN_ON_OFF_TIMER_WITH_OPTIONS_FEATURE;
     }
 
+    /**
+     * Fading options for timer events.
+     */
+    public static enum FadeOption {
+        OFF, ONE_SECOND, ONE_MINUTE, TEN_MINUTES;
+    }
 }
